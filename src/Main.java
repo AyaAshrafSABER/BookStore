8ae71a8c500a8e5ec886c5fd821ce1ad6565938d
Aya Osman <ayasaberosman@gmail.com>
2019-05-08 13:50:08 +0200

    commit all project folders

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,21 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 public class Main extends Application {
+    private static final String  PROFILE = "View/ProfileView.fxml";
+    private static final String  EDIT_PROFILE = "View/EditProfileView.fxml";
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("View/ProfileView.fxml"));
+  //      Parent root = FXMLLoader.load(getClass().getResource(""));
         primaryStage.setTitle("Profile");
-        primaryStage.setScene(new Scene(root, 300, 275));
+   //     primaryStage.setScene(new Scene(root, 400, 600));
+
+        Parent edit = FXMLLoader.load(getClass().getResource(PROFILE));
+        Scene scene = new Scene(edit, 400, 600);
+        primaryStage.setScene(scene);
+       // scene.getStylesheets().add(getClass().getResource("View/DarkTheme.css").toExternalForm());
         primaryStage.show();
+
     }
 
 
