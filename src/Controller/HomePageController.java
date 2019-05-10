415c77964c1a4570b81306b07f574871915af35d
Aya Osman <ayasaberosman@gmail.com>
2019-05-10 08:21:17 +0200

    ADD Views still Shopping cart, Check out and Manager View not complete

--- src/Controller/HomePageController.java
+++ src/Controller/HomePageController.java
@@ -1,4 +1,43 @@
 package Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+import java.awt.event.ActionEvent;
+
 public class HomePageController {
+
+    @FXML
+    private ChoiceBox<?> categories;
+
+    @FXML
+    private TextField isbn;
+
+    @FXML
+    private TextField publisherName;
+
+    @FXML
+    private TextField publicationYear;
+
+    @FXML
+    private TextField price;
+
+    @FXML
+    private TextField authorName;
+
+    @FXML
+    private ComboBox<?> authorsList;
+
+    @FXML
+    void addAuthor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteAuthor(ActionEvent event) {
+
+    }
+
 }
---
+++ src/Controller/PromoteUserController.java
@@ -0,0 +1,14 @@
+package Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class PromoteUserController {
+
+
+    @FXML
+    void initialize() {
+
+    }
+}
